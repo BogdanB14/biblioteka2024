20178aa09fee2778ce0f6a5dd18a874be51d58c9
BogdanB14 <bogdanblagojevic001@gmail.com>
2024-03-07 16:30:41 +0100

    Dodata je logicka kontrola u Klasu bibloteka na drugi nacin

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -2,6 +2,8 @@ package biblioteka;
 
 import java.util.LinkedList;
 
+import javax.management.RuntimeErrorException;
+
 import biblioteka.interfejs.BibliotekaInterface;
 
 public class Biblioteka implements BibliotekaInterface {
@@ -9,6 +11,10 @@ public class Biblioteka implements BibliotekaInterface {
 	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
 	@Override
 	public void dodajKnjigu(Knjiga k) {
+		if(k == null)
+			throw new NullPointerException("Knjiga ne sme biti null");
+		if(knjige.contains(k))
+			throw new RuntimeException("Knjiga vec postoji");
 		knjige.add(k);
 	}
 
@@ -24,6 +30,8 @@ public class Biblioteka implements BibliotekaInterface {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
+		if(autor == null && isbn == null && naslov == null && izdavac == null)
+			return knjige;
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
 		for(Knjiga knjiga : knjige) {
 			if(knjiga.getNaslov().toLowerCase().contains(naslov.toLowerCase()))
